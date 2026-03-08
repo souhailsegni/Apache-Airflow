e25393906acad4e6c10d4b7e9f2b46ee4f23a9e8
souhailsegni <contact.souhail.segni@gmail.com>
2026-03-08 02:01:21 +0100

    create my dag with 3 tasks

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -1,14 +1,50 @@
-from airflow.decorators import dag
-from datetime import datetime, timedelta
+from airflow.decorators import dag, task
+from airflow.hooks.base import BaseHook
+from airflow.sensors.base import PokeReturnValue
+from airflow.operators.python import PythonOperator
+from airflow.providers.docker.operators.docker import DockerOperator
+from datetime import datetime
+
+from include.stock_market.tasks import _get_stock_prices, _store_prices
+
+SYMBOL = 'NVDA'
 
 @dag(
-    start_date=datetime(2026, 3, 5),
+    start_date=datetime(2023, 1, 1),
     schedule='@daily',
     catchup=False,
-    tags=['stock_market']
-)
+    tags=['stock_market'],
 
+)
 def stock_market():
-    pass
+    
+    @task.sensor(poke_interval=30, timeout=300, mode='poke')
+    def is_api_available() -> PokeReturnValue:
+        import requests
+        
+        api = BaseHook.get_connection('stock_api')
+        url = f"{api.host}{api.extra_dejson['endpoint']}"
+        print(url)
+        response = requests.get(url, headers=api.extra_dejson['headers'])
+        condition = response.json()['finance']['result'] is None
+        return PokeReturnValue(is_done=condition, xcom_value=url)
+    
+    get_stock_prices = PythonOperator(
+        task_id='get_stock_prices',
+        python_callable=_get_stock_prices,
+        op_kwargs={'url': '{{ ti.xcom_pull(task_ids="is_api_available") }}', 'symbol': SYMBOL}
+    )
+    
+    store_prices = PythonOperator(
+        task_id='store_prices',
+        python_callable=_store_prices,
+        op_kwargs={'stock': '{{ ti.xcom_pull(task_ids="get_stock_prices") }}'}
+    )
+    
+   
+    
+    is_api_available() >> get_stock_prices >> store_prices
+        
 
 stock_market()
+    
